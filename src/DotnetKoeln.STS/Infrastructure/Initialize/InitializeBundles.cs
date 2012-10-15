a34c003992e288d609599333eaaa4b6001d80858
Albert Weinert <info@der-albert.com>
2012-10-16 00:25:38 +0200

    updated nuget packages and asp.net mvc 4 rtm

---
+++ src/DotnetKoeln.STS/Infrastructure/Initialize/InitializeBundles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Optimization;
+using Aperea.Infrastructure.Bootstrap;
+
+namespace DotnetKoeln.STS.Infrastructure.Initialize
+{
+    public class InitializeBundles : BootstrapItem
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/resources/js").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/modernizr-*",
+                "~/Scripts/jquery.unobtrusive*"
+                            ));
+            bundles.Add(new StyleBundle("~/resources/css").Include("~/Content/site.css"));
+
+        }
+
+        public override void Execute()
+        {
+            RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
